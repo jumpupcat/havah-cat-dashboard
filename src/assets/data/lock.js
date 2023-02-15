ba6bb48049f1c8ec4d6cf2be74b559c63a3ed09c
jumpupcat <jumpupmoon@gmail.com>
2023-02-15 22:51:53 +0900

    get lock data

---
+++ src/assets/data/lock.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const oneSecSleep = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+(async () => {
+    let lock = JSON.parse(fs.readFileSync('src/assets/data/lock.json'));
+
+    let targetDay = lock.length;
+    let lastDay = 0;
+    do {
+        const result = await axios.post(
+            'https://havah.io/api/icon/rewardDashboard',
+            { sequence: targetDay }
+        );
+        
+        lastDay = result.data.message.dayAll;
+        if(result.data.message.msg.result.unrewarded) {
+            lock[targetDay] = result.data.message.msg.result.unrewarded;
+        }
+
+        targetDay++;
+
+        await oneSecSleep();
+    } while(targetDay != lastDay);
+
+    fs.writeFileSync('src/assets/data/lock.json', JSON.stringify(lock, null, 4));
+})()
